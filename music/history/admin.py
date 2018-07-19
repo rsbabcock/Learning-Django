6c77a3c8ece440f1e536e457a83f8b2eaac0e08e
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-19 16:08:47 -0500

    added admin

--- music/history/admin.py
+++ music/history/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song, Artist
+
+
+class SongInline(admin.StackedInline):
+    model = Song
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['artist_name']}),
+        ('Date information', {'fields': ['est_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [SongInline]
+
+admin.site.register(Artist, ArtistAdmin)
