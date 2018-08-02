dde10603792cf732ed6de76dcb5d50a7162361b8
rsbabcock <rachael.s.babcock@gmail.com>
2018-08-02 11:04:26 -0500

    updating models to include album and genre, also added sql file for easy insertions after migrations

--- music/history/models.py
+++ music/history/models.py
@@ -9,8 +9,22 @@ class Artist(models.Model):
 
 
 class Song(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    song_text = models.CharField(max_length=200)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=200)
     song_length = models.IntegerField(default=0)
     def __str__(self):
         return self.song_text
+
+class Album(models.Model):
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+    album_name = models.CharField(max_length=200)
+    album_length = models.IntegerField(default=0)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.album_name
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.genre_name
