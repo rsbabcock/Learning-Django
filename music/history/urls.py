14753490b42a756ff2afdd1c20eded0f9cc03640
rsbabcock <rachael.s.babcock@gmail.com>
2018-08-02 15:21:27 -0500

    getting therer

--- music/history/urls.py
+++ music/history/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /polls/5/
     path('<int:artist_id>/', views.artist, name='artist'),
     
-    path('<int:artist_id>/albums/', views.song_view, name='song_view')
+    path('<int:artist_id>/album/<int:album_id>/', views.song_view, name='album')
     # ex: /polls/5/results/
     # path('<int:artist_id>/song/', views.songs, name='songs'),
 ]
